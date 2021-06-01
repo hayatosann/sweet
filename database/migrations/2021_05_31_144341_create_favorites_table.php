d49602853cda8237fc0928285ed74f9c31c8e6f9
ArisaNAKAMURA <arisanakamura.eng@gmail.com>
2021-06-01 13:13:10 +0900

    外部キーのカラム型の修正 Prefectureテーブルのarea_idのコメントを追加

--- database/migrations/2021_05_31_144341_create_favorites_table.php
+++ database/migrations/2021_05_31_144341_create_favorites_table.php
@@ -15,9 +15,9 @@ class CreateFavoritesTable extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->bigInteger('store_id');
+            $table->unsignedBigInteger('store_id');
             $table->foreign('store_id')->references('id')->on('stores');
             $table->timestamps();
         });
