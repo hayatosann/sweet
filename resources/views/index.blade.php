72e2c2b6f7c7b825516b508b643dab4367be4260
MamikoNakagawa <mamiko.yanagida@gmail.com>
2021-06-02 15:20:35 +0700

    index_add
    indexのHTMLを記入しました

---
+++ resources/views/index.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+    </header>
+
+    <div class="container">
+        <div class="subject">
+            <div class="title">
+                <h1>検索結果</h1>
+            </div>
+            <div class="sarch">
+                <p>検索条件をリセット</p>
+            </div>
+            <div class="choice">
+                <p>指定条件</p>
+            </div>
+        </div>
+        <div class="review">
+            <div class="storephoto">
+                <img src="" alt="店舗写真">
+            </div>
+            <div class="add">
+                <div class="ward">
+                    <p>お気に入り追加</p>
+                </div>
+                <a href=""><img src="" alt="お気に入り星"></a>
+                <a href=""><img src="" alt="口コミ鉛筆"></a>
+                <div class="ward">
+                    <p>口コミ投稿する</p>
+                </div>
+            </div>
+            <div class="info">
+                <p>店舗名引っ張る</p>
+                <p>住所引っ張る</p>
+                <p>カテゴリー引っ張る</p>
+                <p>予算引っ張る</p>
+                <p>評価のポイント平均引っ張る</p>
+                <a href="">詳細</a>
+            </div>
+            <div class="comment">
+                <p>（ユーザーネーム引っ張る）さんの口コミ</p>
+                <p>投稿日時：（create時間引っ張る）</p>
+                <p>評価引っ張る</p>
+                <p>コメント引っ張る</p>
+                <a href="">続きを見る・・・</a>
+                
+            </div>
+
+        </div>
+    </div>
+
+
+</body>
+</html>
