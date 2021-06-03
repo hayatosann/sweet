704a1bec65f26f3ec316bd69593dcc1749d1203a
Mikiko Noro <miriks818@gmail.com>
2021-06-03 17:38:38 +0900

    Revert "口コミ投稿／編集ページ"
    This reverts commit c213ab08e7ddac284b680470f67c540c35bd91b9.

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// views > reviewsフォルダ内確認用Route
-Route::get('/shop_detail', function () {
-    return view('reviews/review_detail');
-});
-Route::get('/review_create', function () {
-    return view('reviews/review_create');
-});
