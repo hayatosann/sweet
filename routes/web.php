052d4e6efc4db5c1ac53fbb61c3d305939bc0c2e
Mikiko Noro <miriks818@gmail.com>
2021-06-03 18:59:32 +0900

    口コミ投稿／編集ページ ・ブラウザ確認のためweb.phpにルーティング記載 ・CSSファイルを2つ追加(style.cssとreset.css) ・viewsフォルダにreviewsフォルダ作成

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// views > reviewsフォルダ内確認用Route
+Route::get('/shop_detail', function () {
+    return view('reviews/review_detail');
+});
+Route::get('/review_create', function () {
+    return view('reviews/review_create');
+});
