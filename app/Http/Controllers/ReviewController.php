212dcd70dbfcd5ef0cca60559dfaa5c0a65d1989
Atsuko Inoue <sinba.805i@gmail.com>
2021-06-10 01:53:46 +0700

    ReviewController@Createの修正、 Review.phpの削除、Conflictになっている部分の解消
    ReviewController@Createにお店情報を返す
    Review.php 空ファイルが上がっていたので削除
    Controller内のconflict部分の修正
    
    // 下書きか公開か、はviewから何かフラグをもらわないとですね！👀
    // そのフラグによって時間を入れるか入れないか、にifで切り分けましょう！
    は未対応です。

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Auth;
+use App\Review;
+use App\Store;
 
 class ReviewController extends Controller
 {
@@ -23,8 +25,26 @@ class ReviewController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request, $id)
     {
+        $store = Store::find($id);
+        
+        $store -> store_id = $request -> store_id;
+        $store -> name = $request -> name;
+        $store -> category_id = $request -> category_id;
+        $store -> address = $request -> address;
+        $store -> opening_hour = $request -> opening_hour;
+        $store -> closing_day = $request -> closing_day;
+        $store -> phone_number = $request -> phone_number;
+        $store -> url = $request -> url;
+        $store -> price = $request -> price;
+        $store -> category_id = $request -> category_id;
+        $store -> prefecture_id = $request -> prefecture_id;
+        $store -> store_image = $request -> store_image;
+        $store -> latitude = $request -> latitude;
+        $store -> longitude = $request -> longitude;
+        $store -> save();
+
         return view('reviews.create');
     }
 
@@ -38,7 +58,6 @@ class ReviewController extends Controller
     {
         $review = new Review;
 
-        $review -> id;
         $review -> store_id = $request -> store_id;
         $review -> comment = $request -> comment;
         $review -> created_at = $request -> created_at;
@@ -48,12 +67,12 @@ class ReviewController extends Controller
         $review -> ate_thing = $request -> ate_thing;
         $review -> category_id = $request -> category_id;
         $review -> post_image = $request -> post_image;
-        $review -> published_at = $request -> published_at;
+        $review -> published_at = now();
         $review -> updated_at = $request -> updated_at;
 
         $review -> save();
         
-        $review = Review::All($id);
+        $review = Review::find($id);
 
         return redirect()->route('stores.show');
 
