e41da22e9f51aa689ecd9d5c5e0f29351684472d
Atsuko Inoue <sinba.805i@gmail.com>
2021-06-10 18:15:15 +0700

    StoreController@Store 画像保存の追加
    ファイルを受けとって、ファイルに保存する。
    
    参考資料；https://qiita.com/Sub_Tanabe/items/74feb9ecf9f0b40702f2

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -39,7 +39,28 @@ class StoreController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $review = new Review;
+        $review -> id = $request -> id;
+        $review -> store_id = $request -> store;
+        $review -> comment = $request -> comment;
+        $review -> ate_thing = $request -> ate_thing;
+        $review -> charge = $request -> charge;
+        $review -> category_id = $request -> category_id;
+        $review -> review = $request -> review;
+        $review -> post_image = $request -> post_image;
+        $review -> user_id = Auth::id();
+
+        $image = $request->file('post_image');
+        // file()で受け取る
+        if($request->file('post_image')->isValid()){
+            $image_name = $image->getClientOriginalName();
+            $review -> post_image = $image->storeAs('public/images', $image_name);
+        }
+
+        $review -> save();
+        dd($review);
+        return redirect()->route('stores.show');
+
     }
 
     /**
@@ -50,7 +71,16 @@ class StoreController extends Controller
      */
     public function show($id)
     {
-        //
+        $review = Review::find($id);
+        return view('reviews.show',['review'=>$review]);
+    
+
+    //  https://qiita.com/Sub_Tanabe/items/74feb9ecf9f0b40702f2
+        // 必要なのは 口コミの作成時に内容&画像を保存できる 機能なので
+        // 書く場所は ReviewController@store
+        //画像は直接DBに保存させるので、Sessionを使わない
+
+        
     }
 
     /**
