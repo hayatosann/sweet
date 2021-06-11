e9bdf16d70090fdedb3e80e9908398d1f88e2cdc
Taisei Namekawa <s.namety.0106.hta1@gmail.com>
2021-06-11 16:14:06 +0900

    退会機能の編集 ・usersテーブルのnameカラムにユニーク制約を追加 ・マイグレーションのカラム名を参考サイトのusernameからnameに変更

--- database/migrations/2021_06_08_093253_drop_unique_to_users.php
+++ database/migrations/2021_06_08_093253_drop_unique_to_users.php
@@ -18,7 +18,7 @@ class DropUniqueToUsers extends Migration
         Schema::table('users', function (Blueprint $table) {
             // ユーザーテーブルからユニークキーを削除
             $table->dropUnique('users_email_unique');
-            $table->dropUnique('users_username_unique');
+            $table->dropUnique('users_name_unique');
         });
     }
 
@@ -32,7 +32,7 @@ class DropUniqueToUsers extends Migration
         Schema::table('users', function (Blueprint $table) {
             // ユニークキー追加
             $table->unique('email', 'users_email_unique');
-            $table->unique('username', 'users_username_unique');
+            $table->unique('name', 'users_name_unique');
         });
     }
 }
