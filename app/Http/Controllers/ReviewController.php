bf7d2a2a7822706f43e89ce0b30f47dad64483c8
Atsuko Inoue <sinba.805i@gmail.com>
2021-06-11 18:10:36 +0700

    画像保存のメソッド記入先を変更
    StoreController@Store -> ReviewController@Store

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -37,7 +37,27 @@ class ReviewController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $review = new Review;
+        $review -> id = $request -> id;
+        $review -> store_id = $request -> store;
+        $review -> comment = $request -> comment;
+        $review -> ate_thing = $request -> ate_thing;
+        $review -> charge = $request -> charge;
+        $review -> category_id = $request -> category_id;
+        $review -> review = $request -> review;
+        $review -> post_image = $request -> post_image;
+        $review -> user_id = Auth::id();
+
+        $image = $request->file('post_image');
+        // file()で受け取る
+        if($request->file('post_image')->isValid()){
+            $image_name = $image->getClientOriginalName();
+            $review -> post_image = $image->storeAs('public/images', $image_name);
+        }
+
+        $review -> save();
+
+        return redirect()->route('stores.show');
     }
 
     /**
