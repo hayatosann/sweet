7959456a6137ff3fdcfbae1ad6ea6431dfc24f4b
tomoko fujii <tomomanma@gmail.com>
2021-06-16 05:31:18 +0900

    edit.blade.php 訂正　 if分まで入れちゃいました　　夜遅くご指導有難うございます！

--- resources/views/Reviews/edit.blade.php
+++ resources/views/Reviews/edit.blade.php
@@ -123,13 +123,13 @@
                 @method('put')
                 <div class="rate">
                     <p>評価(★)</p>
-                    <select name="rating">
-                        <option value="star">☆☆☆☆☆</option>
-                        <option value="star">★☆☆☆☆</option>
-                        <option value="star">★★☆☆☆</option>
-                        <option value="star">★★★☆☆</option>
-                        <option value="star">★★★★☆</option>
-                        <option value="star">★★★★★</option>
+                    <select name="review">
+                        <option value="0" @if($review->rating ==0)selected @endif>☆☆☆☆☆</option>
+                        <option value="1" @if($review->rating ==1)selected @endif>★☆☆☆☆</option>
+                        <option value="2" @if($review->rating ==2)selected @endif>★★☆☆☆</option>
+                        <option value="3" @if($review->rating ==3)selected @endif>★★★☆☆</option>
+                        <option value="4" @if($review->rating ==4)selected @endif>★★★★☆</option>
+                        <option value="5" @if($review->rating ==5)selected @endif>★★★★★</option>
                     </select>
                 </div>
                 <div class="form-group">
@@ -146,30 +146,30 @@
                 </div>
                 <div class="form-group">
                     <label class="space">カテゴリ</label>
-                    <select id="sweets">
-                        <option>ケーキ</option>
-                        <option>タルト、パイ</option>
-                        <option>プリン</option>
-                        <option>チョコレート</option>
-                        <option>シュークリーム</option>
-                        <option>クッキー</option>
-                        <option>アイスクリーム、シャーベット</option>
-                        <option>クレープ</option>
-                        <option>パフェ</option>
-                        <option>その他</option>
+                    <select id="sweets" name="categories">
+                        <option value="category_id" selected @if($review->category_id == 1)selected @endif>ケーキ</option>
+                        <option value="category_id" selected @if($review->category_id == 2)selected @endif>タルト、パイ</option>
+                        <option value="category_id" selected @if($review->category_id == 3)selected @endif>プリン</option>
+                        <option value="category_id" selected @if($review->category_id == 4)selected @endif>チョコレート</option>
+                        <option value="category_id" selected @if($review->category_id == 5)selected @endif>シュークリーム</option>
+                        <option value="category_id" selected @if($review->category_id == 6)selected @endif>クッキー</option>
+                        <option value="category_id" selected @if($review->category_id == 7)selected @endif>アイスクリーム、シャーベット</option>
+                        <option value="category_id" selected @if($review->category_id == 8)selected @endif>クレープ</option>
+                        <option value="category_id" selected @if($review->category_id == 9)selected @endif>パフェ</option>
+                        <option value="category_id" selected @if($review->category_id == 10)selected @endif>その他</option>
                     </select>
                 </div>
                 <div class="low">
                     <div class="form-group picture">
                         <label class="space">写真</label>
                         <label>
-                            <input ref="photo" type="file" class="form-control" value="{{$review -> published_at}}" name="published_at" accept="image/gif,image/jpeg,image/png"
+                            <input ref="photo" type="file" class="form-control" value="{{$review ->published_at}}" name="post_image" accept="image/gif,image/jpeg,image/png"
                                 @change="onFileChange" multiple>
                         </label>
                     </div>
                     <div class="form-group">
                         <label class="space">投稿日時</label>
-                        <input type="date" name="update">
+                        <input type="date" name="update_at">
                     </div>
                     {{-- アクションボタンたち --}}
                     <div class="buttons">
@@ -177,6 +177,7 @@
                             <div class="text-right">
                                 <button type="submit" class="btn overwrite">上書きして保存</button>
                             </div>
+                        </form>
                             <div class="text-right">
                                 <form action="{{ route('reviews.destroy',$review->id)}}" method='post'>
                                     @csrf   
@@ -184,18 +185,18 @@
                                 <button type="submit" class="btn delete">口コミを削除する</button>
                                 </form>  
                             </div>
-                            <div class="spacer"></div>
+                            {{-- <div class="spacer"></div>
                             <div class="text-right">
                                 <button type="submit" class="btn post">口コミを投稿する</button>
                             </div>
                             <div class="text-right">
                                 <button type="submit" class="btn save-draft">下書きに保存</button>
-                            </div>
+                            </div> --}}
                         </div>{{-- action-button --}}
                     </div>{{-- buttons --}}
                 </div>
                 {{-- low --}}
-            </form>
+            
         </div>{{-- review-form --}}
     </section>{{-- bottom-menu --}}
 </body>
