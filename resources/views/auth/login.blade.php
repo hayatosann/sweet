321051e89a29e552305a28dbe37bdd4faca9f918
Taisei Namekawa <s.namety.0106.hta1@gmail.com>
2021-06-15 21:09:35 +0900

    登録、ログインページの修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,26 @@
-@extends('layouts.app')
+@extends('layouts.header')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<body class="allbackground">
+<h2 class="welcome">ログインページへようこそ！</h2>
+<div class="login-container">
+    <div class="login-box justify-content-center register-all">
+        <div class="loginbody">
+            <div class="card ">
+             
+                    <div class="card-header register-name">{{ __('登録済みの方ログイン') }}</div>
+                
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="login-email">
+                            <label for="email" class="email-label">{{ __('メールアドレス') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <div class="login-form">
+                                <input id="email" type="email" class="email-form form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -25,11 +30,11 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <div class="login-pass">
+                            <label for="password" class="pass-label">{{ __('パスワード') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <div class="login-form">
+                                <input id="password" type="password" class="pass-form form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -39,27 +44,27 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <div class="login-pass-check">
+                            <div class="passcheck">
+                                <div class="passform-check">
+                                    <input class="passform-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                    <label class="passform-check-label" for="remember">
+                                        {{ __('パスワードを保存する') }}
                                     </label>
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                            <div class="loginbutton-box">
+                                <button type="submit" class="login-button">
+                                    {{ __('ログイン') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                    <a class="password-forget" href="{{ route('password.request') }}">
+                                        {{ __('パスワードを忘れてしまった場合') }}
                                     </a>
                                 @endif
                             </div>
@@ -70,4 +75,5 @@
         </div>
     </div>
 </div>
+</body>
 @endsection
